e97e0a3e27fa07aa8bdbd363375c7d26f45c6c16
Ch3did <kphch3did@gmail.com>
2025-08-07 21:00:07 -0300

    fix: solve address

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/portifolio/',
+  base: '/me/',
   plugins: [react()],
   resolve: {
     alias: {
