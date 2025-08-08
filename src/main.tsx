0aff143ccc7c18adc9c3f87734f56d8ab224c2c4
Ch3did <kphch3did@gmail.com>
2025-08-07 21:11:04 -0300

    wip: adust main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import "./index.css"
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename="/me/">
     <App />
   </BrowserRouter>
