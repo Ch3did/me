106b5ff708c70098c6021b45eebcf239d2909b16
Ch3did <kphch3did@gmail.com>
2026-07-04 11:36:10 -0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+ import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
+  import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/me/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+  export default defineConfig({
+    plugins: [react()],
+    base: '/me/',
+    server: {
+      host: true,
+      allowedHosts: ['ch3did.com.br', 'www.ch3did.com.br', 'me.ch3did.com.br'],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     }
-  }
-})
+  })
